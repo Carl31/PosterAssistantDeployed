3558e399ae29d7cb67fbae9f480c911d44a91c25
Carl1131 <carlsantos31@yahoo.com>
2025-01-13 04:07:03 +1300

    CORS updated

--- backend/api/app.js
+++ backend/api/app.js
@@ -28,6 +28,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204); // Preflight request
+  }
+  next();
+});
+
 
 // Connect to the database before setting up routes
 // START SERVER: If running locally, use app.listen
