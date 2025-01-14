7c48d56eb9b94769af2d93326f889524a6f4d7a0
Carl1131 <carlsantos31@yahoo.com>
2025-01-15 09:09:10 +1300

    Secured backend CORS

--- backend/api/getObjectData.js
+++ backend/api/getObjectData.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
         }
     } else if (req.method === 'OPTIONS') {
         // Handle the preflight request
-        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins or specify specific origins
+        res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`); // Allow all origins or specify specific origins
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
         res.status(200).end(); // Respond with 200 OK and terminate the response
