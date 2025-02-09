bc2579700a019b23bb4955fe8f3fcf576dd6dc56
Carl1131 <carlsantos31@yahoo.com>
2025-02-09 20:40:10 +1300

    bug fix

--- backend/api/notify.js
+++ backend/api/notify.js
@@ -65,7 +65,7 @@ export default async function handler(req, res) {
     //   res.status(404).json({ message: 'No data available' });
     // }
 
-    res.status(200).json(latestJson);
+    res.status(200).send({ data: latestJson });
 
     latestJson = null;
 
