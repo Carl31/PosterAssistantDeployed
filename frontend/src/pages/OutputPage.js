5e65503807518560d80b369c0eb44171edfd4b04
Carl1131 <carlsantos31@yahoo.com>
2025-02-10 03:05:33 +1300

    debugging

--- frontend/src/pages/OutputPage.js
+++ frontend/src/pages/OutputPage.js
@@ -43,7 +43,8 @@ const OutputPage = () => {
             <div className="pt-4 flex flex-col items-center gap-4">
                 {/* First two buttons inside the same div */}
                 <div className="flex justify-center gap-2">
-                    {location.posterLinks}
+                    {links},
+                    {posterLinks}
                 </div>
             </div>
 
